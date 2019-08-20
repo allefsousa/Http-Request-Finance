37b6066c1f865da671f5d28bd707fc3907ca0ed4
Allef sousa <allefsousa_1@hotmail.com>
2019-08-19 22:01:33 -0300

    Buscando dados em um serviço remoto e os exibindo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -8,8 +7,12 @@ const request = "https://api.hgbrasil.com/finance?format=json&key=622d376f";
 
 void main() async {
   runApp(MaterialApp(
-    home: Home(),
-  ));
+      home: Home(),
+      theme: ThemeData(
+          inputDecorationTheme: InputDecorationTheme(
+              enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.white),
+      )))));
 }
 
 Future<Map> getData() async {
@@ -23,6 +26,42 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final realController = TextEditingController();
+  final dolarController = TextEditingController();
+  final euroController = TextEditingController();
+
+  double dolar;
+  double euro;
+
+  void _realChanged(String text){
+    if(text.isEmpty){
+      _clearAll();
+          return;
+    }
+    double real = double.parse(text);
+    dolarController.text = (real/dolar).toStringAsFixed(2);
+    euroController.text = (real/euro).toStringAsFixed(2);
+  }
+  void _dolarChanged(String text){
+    if(text.isEmpty){
+      _clearAll();
+      return;
+    }
+    double dolar = double.parse(text);
+    realController.text = (dolar *this.dolar).toStringAsFixed(2);
+    euroController.text = (dolar *this.dolar/euro).toStringAsFixed(2);
+  }
+  void _euroChanged(String text){
+    if(text.isEmpty){
+      _clearAll();
+      return;
+    }
+    double euro = double.parse(text);
+    realController.text =(euro*this.euro).toStringAsFixed(2);
+    dolarController.text =(euro*this.euro/dolar).toStringAsFixed(2);
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,10 +92,47 @@ class _HomeState extends State<Home> {
                     textAlign: TextAlign.center,
                   ));
                 } else {
-                  return Container(color: Colors.green,);
+                  dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                  euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+                  return SingleChildScrollView(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Icon(Icons.monetization_on,
+                            size: 150.0, color: Colors.amber),
+                      buildTextField("Reais","R\$",realController,_realChanged),
+                        Divider(),
+                      buildTextField("DOLARES","US\$",dolarController,_dolarChanged),
+                        Divider(),
+                      buildTextField("EURO","E",euroController,_euroChanged),
+                      ],
+                    ),
+                  );
                 }
             }
           }),
     );
   }
+
+  Widget buildTextField(String label,String prefix,TextEditingController control,Function func){
+    return  TextField(
+      controller: control,
+      decoration: InputDecoration(
+          labelText: label,
+          labelStyle: TextStyle(color: Colors.amber),
+          border: OutlineInputBorder(),
+          prefixText: prefix),
+      style: TextStyle(color: Colors.amber, fontSize: 25.0),
+      keyboardType: TextInputType.number,
+      onChanged: func,
+    );
+  }
+  void _clearAll(){
+    realController.text = "";
+    dolarController.text = "";
+    euroController.text = "";
+  }
+
+
 }
